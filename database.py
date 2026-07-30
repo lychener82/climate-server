3141cf3be2143fbb86af7c1352573b9143370cd0
lychener82 <lychener82@proton.me>
2026-07-30 21:53:05 +0200

    Refactor database.py to include sessionmaker

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy import text
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    expire_on_commit=False
 )
 
 
@@ -16,4 +23,4 @@ def test_connection():
             text("SELECT version();")
         ).scalar()
 
-        print(version)
+        print(version, flush=True)
