4489b1cbd63d274bae29427609c5385c465a9b26
lychener82 <lychener82@proton.me>
2026-07-30 21:17:28 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,19 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
-engine=create_engine(DATABASE_URL,pool_pre_ping=True)
-SessionLocal=sessionmaker(bind=engine)
+from sqlalchemy import text
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+
+def test_connection():
+    with engine.connect() as connection:
+        version = connection.execute(
+            text("SELECT version();")
+        ).scalar()
+
+        print(version)
