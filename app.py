786e39e35c248dcc0165b5d4c83981a956b480f6
lychener82 <lychener82@proton.me>
2026-08-01 09:54:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-from flask import Flask, jsonify, render_template, request
-from sqlalchemy import select
-from routes.measurements import bp
+from flask import Flask, render_template
+
 from database import test_connection
-app=Flask(__name__)
+from routes.measurements import bp
+
+
+app = Flask(__name__)
+
 test_connection()
 app.register_blueprint(bp)
+
+
 @app.get("/")
 def home():
     return render_template("index.html")
+
+
+@app.get("/instrument")
+def instrument():
+    return render_template("instrument.html")
+
+
+@app.get("/record")
+def record():
+    return render_template("record.html")
+
+
+@app.get("/registrar")
+def registrar():
+    return render_template("registrar.html")
+
+
 @app.get("/health")
-def health(): return {"status":"ok"}
-if __name__=="__main__": app.run(host="0.0.0.0",port=5000)
+def health():
+    return {
+        "status": "ok"
+    }
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
